be3823545d2c20d86f830dd223bc9186aee58ff5
Oğuzhan <oguzhan.umutlu123@gmail.com>
2022-08-28 16:14:55 +0300

    added asString parameter

--- ConsoleReader.js
+++ ConsoleReader.js
@@ -7,7 +7,6 @@ class ConsoleReader {
 
     resumeStdin() {
         this.stdin.resume();
-        this.stdin.setEncoding("utf8");
     };
 
     pauseStdin() {
@@ -30,32 +29,34 @@ class ConsoleReader {
         this._stdinData = [];
     };
 
-    readLine({show = true} = {}) {
+    readLine({show = true, asString = true} = {}) {
         return new Promise(resolve => {
             this.resumeStdin();
-            let dat = "";
-            const rem = this.onStdinData(data => {
+            let dat = [];
+            const rem = this.onStdinData(dataB => {
+                const data = dataB.toString();
                 if (data === "\x03") return process.exit();
                 if (data[0] === "\n" || data[0] === "\r") {
                     this.pauseStdin();
                     rem.remove();
-                    resolve(dat);
+                    resolve(asString ? dat.map(i => i.toString()).join("") : dat);
                 } else if (data === "\b") {
                     if (dat.length > 0) {
                         dat = dat.substring(0, dat.length - 1);
                         if (show) process.stdout.write("\b ");
                     }
-                } else dat += data;
+                } else dat.push(dataB);
                 if (show) process.stdout.write(data);
             });
         });
     };
 
-    readKey({show = true, amount = 1} = {}) {
+    readKey({show = true, amount = 1, asString = true} = {}) {
         return new Promise(resolve => {
             this.resumeStdin();
             let dat = "";
-            const rem = this.onStdinData(data => {
+            const rem = this.onStdinData(dataB => {
+                const data = dataB.toString();
                 if (data === "\x03") return process.exit();
                 if (data === "\b") {
                     if (dat.length > 0) {
@@ -66,7 +67,7 @@ class ConsoleReader {
                 if (dat.length >= amount) {
                     this.pauseStdin();
                     rem.remove();
-                    resolve(dat);
+                    resolve(asString ? data : dataB);
                 }
                 if (show) process.stdout.write(data);
             });
